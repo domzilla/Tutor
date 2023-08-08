5df670aa1bc0369f122227266a04bfc2f7387b48
Dominic Rodemer <dominic.rodemer@me.com>
2023-08-08 11:16:28 +0200

    Added custom colors

---
+++ src/Tutor/Classes/UIColor+Tutor.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+Tutor.h
+//  Tutor
+//
+//  Created by Dominic Rodemer on 08.08.23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Tutor)
+
++ (UIColor *)mt_redColor;
++ (UIColor *)mt_greenColor;
++ (UIColor *)mt_blueColor;
+
+@end
---
+++ src/Tutor/Classes/UIColor+Tutor.m
@@ -0,0 +1,27 @@
+//
+//  UIColor+Tutor.m
+//  Tutor
+//
+//  Created by Dominic Rodemer on 08.08.23.
+//
+
+#import "UIColor+Tutor.h"
+
+@implementation UIColor (Tutor)
+
++ (UIColor *)mt_redColor
+{
+    return [UIColor colorWithRed:233.0/255 green:54.0/255 blue:100.0/255 alpha:1.0];
+}
+
++ (UIColor *)mt_greenColor
+{
+    return [UIColor colorWithRed:54.0/255 green:233.0/255 blue:138.0/255 alpha:1.0];
+}
+
++ (UIColor *)mt_blueColor
+{
+    return [UIColor colorWithRed:0.0/255 green:108.0/255 blue:255.0/255 alpha:1.0];
+}
+
+@end
